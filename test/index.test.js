bf4e8c4da2489cea0bc6ae8a2a5958116919d226
Arthur Bogaart <a.bogaart@onehippo.com>
2016-11-20 23:42:59 +0100

    Fix code coverage
    Failed to get nyc setting 'all' to work. For now I require all .js files in lib/ with require-all in a todo testcase.
    Also changed the coverage output for the test-ci script to text-summary.

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+
+// require all transpiled .js files to make sure the code-coverage also includes files wihout tests
+// Note: this is a workaround for me not getting the nyc setting "all" to work. Remove once that is fixed.
+require('require-all')(__dirname + '/../lib');
+
+// make ava happy
+test.todo('all test files required');
+
