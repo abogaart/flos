098461a8e4699f361ca6322163c6f8e6d21fe052
Arthur Bogaart <a.bogaart@onehippo.com>
2016-11-28 01:29:13 +0100

    Simple CLI client

--- bin/flos.js
+++ bin/flos.js
@@ -3,5 +3,16 @@
 /* eslint-disable no-var */
 'use strict';
 
+const flos = require('../lib/index');
+const FlosRunner = flos.Runner;
+
 // load config
-// start flos
+const config = {};
+// load linters
+const linters = [];
+// create runner
+const runner = new FlosRunner(linters);
+// configure runner
+runner.configure(config);
+// flos
+runner.run();
