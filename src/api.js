025b6aea2089a5fe11ac47878a0dd63285b34ec1
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-01-01 03:53:20 +0100

    Enhance API run function and hide console.log messages

--- src/api.js
+++ src/api.js
@@ -8,8 +8,8 @@ export const Processor = FlosProcessor;
 export const Reporter = FlosReporter;
 export const Runner = FlosRunner;
 
-export function run(linters, config) {
+export function run(linters, config, reporter = new Reporter(), processor = new Processor()) {
   const runner = new FlosRunner(linters);
   runner.configure(config);
-  runner.run();
+  runner.run(processor, reporter);
 }
--- test/api.test.js
+++ test/api.test.js
@@ -1,9 +1,13 @@
 import test from 'ava';
-import {run} from '../src/api';
+import sinon from 'sinon';
+import {run, Reporter} from '../src/api';
 
 test('run function', t => {
   const linters = [];
   const config = {};
-  run(linters, config);
+  const reporter = new Reporter();
+  // hide default 'finished' output from test output
+  sinon.stub(reporter, 'print');
+  run(linters, config, reporter);
   t.pass();
 });
