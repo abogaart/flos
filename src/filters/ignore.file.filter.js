7cd36f72b3bf19af4a8ac3b6cbae43a9074a77e5
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-01-01 03:19:14 +0100

    Add ignore-file-filter and tests

--- src/filters/ignore.file.filter.js
+++ src/filters/ignore.file.filter.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
-import path from 'path';
 import shell from 'shelljs';
+import pathUtil from 'path-util';
 import IgnoreFilter from './ignore.filter';
 
 const debug = require('debug')('flos:ignore');
@@ -9,6 +9,8 @@ class IgnoreFileFilter extends IgnoreFilter {
   /**
    * @param {Object} options object containing 'cwd', 'dotfiles',
    *                 'ignore', 'ignorePath', 'ignoreFileName' and 'ignorePatterns' properties
+   * @param {string} [options.ignorePath]  Path to the ignore file
+   * @param {string} [options.ignoreFilename]  Filename of the ignore file
    */
   constructor(options) {
     super(options);
@@ -16,61 +18,27 @@ class IgnoreFileFilter extends IgnoreFilter {
     const ignorePath = this.getIgnorePath();
     if (ignorePath) {
       debug(`Adding ${ignorePath}`);
-      this.baseDir = path.dirname(path.resolve(this.baseDir, ignorePath));
-      debug(`Set baseDir to ${this.baseDir}`);
-      this.addIgnoreFile(ignorePath);
+      this.ignore.add(fs.readFileSync(ignorePath, 'utf8'));
     }
   }
 
-  /**
-   * add ignore file to node-ignore instance
-   * @param {string} filePath, file to add to node-ignore
-   * @returns {array} raw ignore rules
-   */
-  addIgnoreFile(filePath) {
-    this.ignoreFiles.push(filePath);
-    return this.ignore.add(fs.readFileSync(filePath, 'utf8'));
-  }
-
   getIgnorePath() {
-    let ignorePath;
     if (this.options.ignorePath) {
       debug('Using specific ignore file', this.options.ignorePath);
-
       const ignoreFile = this.options.ignorePath;
       try {
         fs.statSync(ignoreFile);
-        ignorePath = ignoreFile;
+        return ignoreFile;
       } catch (err) {
         err.message = `Cannot read ignore file: ${ignoreFile}\nError: ${err.message}`;
         throw err;
       }
     } else {
-      ignorePath = this.findIgnoreFile();
-      try {
-        fs.statSync(ignorePath);
-        debug(`Loaded ignore file ${ignorePath}`);
-      } catch (err) {
-        debug('Could not find ignore file in baseDir');
-      }
+      const ignoreFile = pathUtil.toAbsolute(this.options.ignoreFilename, this.getBaseDir()) || '';
+      debug(`Looking for ignore file with path ${ignoreFile}`);
+      return shell.test('-f', ignoreFile) ? ignoreFile : false;
     }
-    return ignorePath;
   }
-
-  /**
-   * Find an ignore file in the current baseDir.
-   * @returns {string} Path of ignore file or an empty string.
-   */
-  findIgnoreFile() {
-    const ignoreFilePath = path.resolve(this.getBaseDir(), this.options.ignoreFileName || '');
-    debug(`Looking for ignore file with path ${ignoreFilePath}`);
-    return shell.test('-f', ignoreFilePath) ? ignoreFilePath : '';
-  }
-
-  getBaseDir() {
-    return this.baseDir || super.getBaseDir();
-  }
-
 }
 
 export default IgnoreFileFilter;
--- test/filters/ignore.file.filter.test.js
+++ test/filters/ignore.file.filter.test.js
@@ -1,24 +1,43 @@
 /* eslint-disable import/default */
-
 import test from 'ava';
 
 import fixtures from '../fixtures/index';
 import IgnoreFileFilter from '../../src/filters/ignore.file.filter';
 
-let fixture;
-
-test.before(() => {
-  fixture = fixtures.fixture('ignore-file-filter');
-});
-
 test.after.always(() => {
   fixtures.cleanup('ignore-file-filter');
 });
 
-test('sets baseDir to cwd when no ignore file was found', t => {
+test('finds the ignorefile by filename if configured', t => {
+  const fixture = fixtures.fixture('ignore-file-filter');
   const filter = new IgnoreFileFilter({
-    cwd: fixture('no-ignore-file')
+    cwd: fixture(),
+    ignoreFilename: '.flosignore'
   });
+  t.true(filter.apply('file.js'));
+  t.false(filter.apply('file.css'));
+  t.is(filter.getIgnorePath(), fixture('.flosignore'));
+});
 
-  t.is(filter.getBaseDir(), fixture('no-ignore-file'));
+test('can be configured with an absolute path to the ignore file', t => {
+  const fixture = fixtures.fixture('ignore-file-filter');
+  const filter = new IgnoreFileFilter({
+    ignorePath: fixture('.flosignore')
+  });
+  t.true(filter.apply('file.js'));
+  t.false(filter.apply('file.css'));
+  t.is(filter.getIgnorePath(), fixture('.flosignore'));
+});
+
+test('throws an error when option ignorePath is set and file not found', t => {
+  const fixture = fixtures.fixture('ignore-file-filter');
+  const ignorePath = fixture('.noflosignore');
+  const error = t.throws(() => new IgnoreFileFilter({ignorePath}));
+  t.true(error.message.startsWith('Cannot read ignore file: ' + ignorePath));
+});
+
+test('returns false when file referenced by option ignoreFilename is not found', t => {
+  const ignoreFilename = '.noflosignore';
+  const filter = new IgnoreFileFilter({ignoreFilename});
+  t.false(filter.getIgnorePath());
 });
