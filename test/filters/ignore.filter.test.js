8cddfa28978452335de31a9b18fcbedbac4edcae
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-01-01 03:18:26 +0100

    Add ignore filter tests

--- test/filters/ignore.filter.test.js
+++ test/filters/ignore.filter.test.js
@@ -2,8 +2,56 @@ import test from 'ava';
 
 import IgnoreFilter from '../../src/filters/ignore.filter';
 
-test('Ignores dot-files by default', t => {
+test('allows all files except dotfiles by default', t => {
   const filter = new IgnoreFilter();
-  const filtered = filter.apply('.flosrc');
-  t.true(filtered);
+  t.true(filter.apply('.flosrc'));
+  t.false(filter.apply('file.js'));
+  t.false(filter.apply('/path/to/file.js'));
+});
+
+test('allows dot-files by configuration', t => {
+  const filter = new IgnoreFilter({dotfiles: true});
+  t.false(filter.apply('.flosrc'));
+});
+
+test('ignores files by pattern', t => {
+  const filter = new IgnoreFilter({
+    ignorePatterns: '*.js'
+  });
+
+  t.true(filter.apply('file.js'));
+  t.true(filter.apply('/path/file.js'));
+  t.false(filter.apply('file.css'));
+  t.false(filter.apply('/path/file.css'));
+});
+
+test('ignores files by patterns', t => {
+  const filter = new IgnoreFilter({
+    ignorePatterns: ['*.js', '*.css']
+  });
+
+  t.true(filter.apply('file.js'));
+  t.true(filter.apply('file.css'));
+  t.true(filter.apply('/path/file.js'));
+  t.true(filter.apply('/path/file.css'));
+});
+
+test('pattern can be added', t => {
+  const filter = new IgnoreFilter();
+  filter.addPattern('*.js');
+
+  t.true(filter.apply('file.js'));
+  t.true(filter.apply('/path/file.js'));
+  t.false(filter.apply('file.css'));
+  t.false(filter.apply('/path/file.css'));
+});
+
+test('patterns can be added', t => {
+  const filter = new IgnoreFilter();
+  filter.addPattern(['*.js', '*.css']);
+
+  t.true(filter.apply('file.js'));
+  t.true(filter.apply('file.css'));
+  t.true(filter.apply('/path/file.js'));
+  t.true(filter.apply('/path/file.css'));
 });
